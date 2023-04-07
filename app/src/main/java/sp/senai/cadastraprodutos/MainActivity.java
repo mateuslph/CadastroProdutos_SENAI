5dc6fb7543d8c84d49e4f9285285832df65d4328
mateuslph <mlptche@gmail.com>
2023-04-07 16:00:32 -0300

    Front-End Splash Screen

---
+++ app/src/main/java/sp/senai/cadastraprodutos/MainActivity.java
@@ -0,0 +1,13 @@
+package sp.senai.cadastraprodutos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
