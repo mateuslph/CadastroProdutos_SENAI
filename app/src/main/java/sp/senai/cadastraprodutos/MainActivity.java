ea7fa7a1a5284176d898d87d5f874629b3c8c0ea
mateuslph <mlptche@gmail.com>
2023-04-07 16:28:51 -0300

    Codigo Splash Screen

--- app/src/main/java/sp/senai/cadastraprodutos/MainActivity.java
+++ app/src/main/java/sp/senai/cadastraprodutos/MainActivity.java
@@ -1,13 +1,43 @@
 package sp.senai.cadastraprodutos;
 
+import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
+
+    Thread thread;
+    Handler handler;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        i = 1;
+        try {
+            while (i < 100) {
+                Thread.sleep(15);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        i++;
+                    }
+                });
+            }
+        } catch (Exception ex) {
+
+        }
+        finish();
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
