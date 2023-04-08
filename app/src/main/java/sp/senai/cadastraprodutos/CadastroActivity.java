817c8ffd92b16b02e9be29dc0214d04a44bdcf14
mateuslph <mlptche@gmail.com>
2023-04-08 17:04:05 -0300

    Funcionalidade de tirar foto concluida

--- app/src/main/java/sp/senai/cadastraprodutos/CadastroActivity.java
+++ app/src/main/java/sp/senai/cadastraprodutos/CadastroActivity.java
@@ -1,21 +1,40 @@
 package sp.senai.cadastraprodutos;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    private ImageView imageViewFoto;
     private Button btnListar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+
+        imageViewFoto = (ImageView) findViewById(R.id.imageView5);
+        findViewById(R.id.buttonCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tirarFoto();
+            }
+        });
 
         btnListar = findViewById(R.id.btnListar);
         btnListar.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +44,21 @@ public class CadastroActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imagem = (Bitmap) extras.get("data");
+            imageViewFoto.setImageBitmap(imagem);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void inserir(View view) {
